fc3c59bc5f813dfc6e94f9faf3afd8a323b5aec9
kentwilliam95 <kentwilliam95@gmail.com>
2022-10-18 00:15:21 +0700

    Fix GC alloc by updat panelCoins each frame

--- Assets/Cooking/Scripts/Game/UserInterface/PanelCoin.cs
+++ Assets/Cooking/Scripts/Game/UserInterface/PanelCoin.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Core;
 
 namespace Cooking
 {
-    public class PanelCoin : MonoBehaviour
+    public class PanelCoin : MonoBehaviour, EventListener<EventFoodServed>
     {
         public TextMeshProUGUI textUI;
-        private void Update()
+        private void Awake()
+        {
+            EventManager<EventFoodServed>.AddListener(this);    
+        }
+
+        private void OnDestroy()
+        {
+            EventManager<EventFoodServed>.RemoveListener(this);
+        }
+
+        private void Start()
+        {
+            UpdateUI();
+        }
+
+        public void OnInvoke(EventFoodServed evt)
+        {
+            UpdateUI();
+        }
+
+        private void UpdateUI()
         {
             textUI.text = $"{Database.UserData.Coins}";
         }
